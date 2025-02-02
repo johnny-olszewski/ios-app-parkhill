e006a2143bcb54401d0b4ee0ef3be2a6c9ca0678
johnny <johnnyo.dev@gmail.com>
2025-02-02 08:25:21 -0800

    [PH-02][9/n] add optional verses

--- ParkHillChurch/Data Model/BiblePassage.swift
+++ ParkHillChurch/Data Model/BiblePassage.swift
@@ -9,10 +9,11 @@ import Foundation
 
 struct BiblePassage: Codable, Hashable {
     let book: BibleBook
-//    let chapters: [Int]
+    let verses: [Int: VerseRange]?
     
     enum CodingKeys: String, CodingKey {
         case book
+        case verses
     }
     
     init(from decoder: Decoder) throws {
@@ -21,12 +22,29 @@ struct BiblePassage: Codable, Hashable {
         // Decode BiblePassages
         let bookString = try container.decode(String.self, forKey: .book)
         self.book = BibleBook(rawValue: bookString) ?? .unknown
+        
+        self.verses = try container.decodeIfPresent([Int: VerseRange].self, forKey: .verses)
+    }
+    
+    struct VerseRange: Codable, Hashable, CustomStringConvertible {
+        let start: Int
+        let end: Int
+        
+        var description: String { "\(start)-\(end)"}
     }
 }
 
 extension BiblePassage: CustomStringConvertible {
     var description: String {
-        "\(book.rawValue)"
+        var result = "\n"
+        
+        if let verses = self.verses {
+            for chapter in verses {
+                result += "\(book.rawValue) \(chapter.key): \(chapter.value)\n"
+            }
+        }
+        
+        return result
     }
 }
 
