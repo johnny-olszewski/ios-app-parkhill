d952e27a1cdb857ae966e8a2b3cd41f9a3a430cc
johnny <johnnyo.dev@gmail.com>
2025-02-02 07:50:48 -0800

    [PH-02][8/n] add decoding for BiblePassage

--- ParkHillChurch/Data Model/BiblePassage.swift
+++ ParkHillChurch/Data Model/BiblePassage.swift
@@ -8,101 +8,26 @@
 import Foundation
 
 struct BiblePassage: Codable, Hashable {
-    let book: String // BibleBook
+    let book: BibleBook
 //    let chapters: [Int]
+    
+    enum CodingKeys: String, CodingKey {
+        case book
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // Decode BiblePassages
+        let bookString = try container.decode(String.self, forKey: .book)
+        self.book = BibleBook(rawValue: bookString) ?? .unknown
+    }
 }
 
 extension BiblePassage: CustomStringConvertible {
     var description: String {
-        "\(book)"
+        "\(book.rawValue)"
     }
 }
 
-/// Enum representing the 66 books of the Protestant Bible in canonical order.
-enum BibleBook: String, CaseIterable, Codable {
-    // Pentateuch (Torah)
-    case genesis = "Genesis"
-    case exodus = "Exodus"
-    case leviticus = "Leviticus"
-    case numbers = "Numbers"
-    case deuteronomy = "Deuteronomy"
-    
-    // Historical Books
-    case joshua = "Joshua"
-    case judges = "Judges"
-    case ruth = "Ruth"
-    case firstSamuel = "1 Samuel"
-    case secondSamuel = "2 Samuel"
-    case firstKings = "1 Kings"
-    case secondKings = "2 Kings"
-    case firstChronicles = "1 Chronicles"
-    case secondChronicles = "2 Chronicles"
-    case ezra = "Ezra"
-    case nehemiah = "Nehemiah"
-    case esther = "Esther"
-    
-    // Wisdom/Poetry
-    case job = "Job"
-    case psalms = "Psalms"
-    case proverbs = "Proverbs"
-    case ecclesiastes = "Ecclesiastes"
-    case songOfSolomon = "Song of Solomon"  // a.k.a. Song of Songs
-    
-    // Major Prophets
-    case isaiah = "Isaiah"
-    case jeremiah = "Jeremiah"
-    case lamentations = "Lamentations"
-    case ezekiel = "Ezekiel"
-    case daniel = "Daniel"
-    
-    // Minor Prophets
-    case hosea = "Hosea"
-    case joel = "Joel"
-    case amos = "Amos"
-    case obadiah = "Obadiah"
-    case jonah = "Jonah"
-    case micah = "Micah"
-    case nahum = "Nahum"
-    case habakkuk = "Habakkuk"
-    case zephaniah = "Zephaniah"
-    case haggai = "Haggai"
-    case zechariah = "Zechariah"
-    case malachi = "Malachi"
-    
-    // Gospels
-    case matthew = "Matthew"
-    case mark = "Mark"
-    case luke = "Luke"
-    case john = "John"
-    
-    // History (New Testament)
-    case acts = "Acts"
-    
-    // Pauline Epistles
-    case romans = "Romans"
-    case firstCorinthians = "1 Corinthians"
-    case secondCorinthians = "2 Corinthians"
-    case galatians = "Galatians"
-    case ephesians = "Ephesians"
-    case philippians = "Philippians"
-    case colossians = "Colossians"
-    case firstThessalonians = "1 Thessalonians"
-    case secondThessalonians = "2 Thessalonians"
-    case firstTimothy = "1 Timothy"
-    case secondTimothy = "2 Timothy"
-    case titus = "Titus"
-    case philemon = "Philemon"
-    
-    // General Epistles
-    case hebrews = "Hebrews"
-    case james = "James"
-    case firstPeter = "1 Peter"
-    case secondPeter = "2 Peter"
-    case firstJohn = "1 John"
-    case secondJohn = "2 John"
-    case thirdJohn = "3 John"
-    case jude = "Jude"
-    
-    // Prophecy (New Testament)
-    case revelation = "Revelation"
-}
+
